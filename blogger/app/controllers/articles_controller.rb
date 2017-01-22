0c38e1ebbd9334bf865eea2e483b53c106d13dc5
artek2001 <artek2001@gmail.com>
2017-01-22 22:29:58 +0300

    Changes for articles_controller, update articles with form

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -12,11 +14,32 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(
-      title: params[:article][:title],
-      body: params[:article][:body]
-    )
+    @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article #{@article.title} has been created"
+
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+
+    flash.notice = "Article #{@article.title} has been destroyed"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article #{@article.title} has been updated"
+
+    redirect_to articles_path
   end
 end
